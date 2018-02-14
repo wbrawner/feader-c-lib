ab8739262d9734734596dc5092bc7fb6ff3c13e6
Billy Brawner <billybrawner@gmail.com>
2018-02-13 18:28:34 -0600

    Clean up source

--- src/feader.c
+++ src/feader.c
@@ -1,28 +1,12 @@
 #include "curl/curl.h"
 #include "curl/easy.h"
+#include "feader.h"
 #include "libxml/parser.h"
 #include "libxml/tree.h"
 #include "libxml/xmlreader.h"
 #include "libxml/xmlstring.h"
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct {
-    char* data;
-    int size;
-    char* url;
-    char* errBuf;
-} xml;
-
-size_t my_write_callback(char *ptr, size_t size, size_t nmemb, xml *x);
-
-size_t get_xml(xml *x, char *url);
-
-void parse_xml_items(xml *x, xmlDocPtr *doc);
-
-xml x;
-
-xmlDocPtr xdp;
 
 int main(int argc, char **argv) {
     if (argc < 2) {
@@ -31,27 +15,87 @@ int main(int argc, char **argv) {
     } else {
         printf("Attempting to retrieve XML for URL: %s\n", argv[1]);
     }
-    x.size = 0;
-    x.data = malloc(1);
-    x.data[0] = '\0';
-    x.url = argv[1];
 
-    int res = get_xml(&x, x.url);
-    // printf("\n%s\n", x.data);            
+    fead_xml(argv[1]);
+
+    return 0;
+}
+
+void fead_xml(char* url) {
+    xml *x = malloc(sizeof(xml));
+    get_xml_ptr(x, url);
+
+    int res = get_xml(x);
     if (res == CURLE_OK) {
         printf("XML data retrieved from server\n");
     } else {
-        printf("Unable to retrieve XML for URL: %s\n", x.url);
-        printf("%s\n", x.errBuf);
+        printf("Unable to retrieve XML for URL: %s\n", x->url);
+        printf("%s\n", x->errBuf);
     }
 
-    parse_xml_items(&x, &xdp);
+    parse_xml_items(x);
 
-    if (xdp == NULL || xdp->children == NULL) {
+    save_xml_elements(x);
+
+    cleanup_xml(x);
+}
+
+void get_xml_ptr(xml* x, char* url) {
+    x->size = 0;
+    x->data = malloc(1);
+    x->data[0] = '\0';
+    x->url = url;
+}
+
+size_t my_write_callback(char *ptr, size_t size, size_t nmemb, xml *x) {
+    int byte_size = (size * nmemb);
+    printf("byte_size: %d\n", byte_size);
+    int new_size = x->size + byte_size;
+    printf("new_size size: %d\n", new_size);
+    x->data = realloc(x->data, new_size + 1);
+    memcpy(x->data + x->size, ptr, byte_size);
+    x->data[new_size] = '\0';
+    x->size = new_size;
+    printf("New xml size: %d\n", x->size);
+    return byte_size;
+}
+
+size_t get_xml(xml* x) {
+    CURL *curl = curl_easy_init();
+
+    if (!curl) {
+        printf("Unable to instantiate curl object. Aborting");
+        return 1;
+    } 
+
+    curl_easy_setopt(curl, CURLOPT_URL, x->url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, *my_write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, x);
+    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, x->errBuf);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+    CURLcode res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+    return res;
+}
+
+void parse_xml_items(xml *x) {
+    xmlChar* xChar = xmlCharStrndup(x->data, x->size);
+    x->xdp = xmlReadDoc(
+            xChar,
+            x->url,
+            NULL,
+            XML_PARSE_RECOVER
+            );
+    xmlFree(xChar);
+}
+
+void save_xml_elements(xml* x) {
+    if (x->xdp == NULL || x->xdp->children == NULL) {
         printf("Unable to parse XML\n");
     }
 
-    xmlNode* rss = xdp->children;
+    xmlNode* rss = x->xdp->children;
     xmlNode* channel = rss->children->next;
     xmlNode* channelChild = channel->children;
     int articleCount = 0;
@@ -70,59 +114,28 @@ int main(int argc, char **argv) {
             printf("Article info:\n");
             do {
                 if (strncmp("text", itemData->name, strlen(itemData->name)) != 0) {
-                    printf("%s: %s\n", itemData->name, itemData->children->content);
+                    if (strlen(itemData->children->content) > 200) {
+                        printf("%s: %ld chars\n", itemData->name, strlen(itemData->children->content));
+                    } else {
+                        printf("%s: %s\n", itemData->name, itemData->children->content);
+                    }
                 }
                 itemData = itemData->next;
             } while (itemData != NULL);
+            printf("\n");
         }
         channelChild = channelChild->next;
     }
 
-    printf("Found %d articles in the feed\n", articleCount);
+    rss = NULL;
+    channel = NULL;
 
-
-    free(x.data);
-    free(xdp);
-    return 0;
+    printf("Found %d articles in the feed\n", articleCount);    
 }
 
-size_t my_write_callback(char *ptr, size_t size, size_t nmemb, xml *x) {
-    int byte_size = (size * nmemb);
-    printf("byte_size: %d\n", byte_size);
-    int new_size = x->size + byte_size;
-    printf("new_size size: %d\n", new_size);
-    x->data = realloc(x->data, new_size + 1);
-    memcpy(x->data + x->size, ptr, byte_size);
-    x->data[new_size] = '\0';
-    x->size = new_size;
-    printf("New xml size: %d\n", x->size);
-    return byte_size;
-}
-
-size_t get_xml(xml* x, char* url) {
-    CURL *curl = curl_easy_init();   
-
-    if (!curl) {
-        printf("Unable to instantiate curl object. Aborting");
-        return 1;
-    } 
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, *my_write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, x);
-    curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, x->errBuf);
-    CURLcode res = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
-    return res;
-}
-
-void parse_xml_items(xml *x, xmlDocPtr *doc) {
-    xmlChar* xChar = malloc(x->size);
-    xChar = xmlCharStrndup(x->data, x->size);
-    (*doc) = xmlReadDoc(
-            xChar,
-            x->url,
-            NULL,
-            XML_PARSE_RECOVER
-            );
-    free(xChar);
+void cleanup_xml(xml* x) { 
+    free(x->data);
+    free(x->errBuf);
+    xmlFreeDoc(x->xdp);
+    free(x);
 }
---
+++ src/feader.h
@@ -0,0 +1,24 @@
+#include "libxml/tree.h"
+
+typedef struct {
+    char* data;
+    int size;
+    long totalSize;
+    char* url;
+    char* errBuf;
+    xmlDocPtr xdp;
+} xml;
+
+size_t my_write_callback(char *ptr, size_t size, size_t nmemb, xml *x);
+
+size_t get_xml(xml *x);
+
+void parse_xml_items(xml *x);
+
+void get_xml_ptr(xml* x, char* url);
+
+void save_xml_elements(xml* x);
+
+void cleanup_xml(xml* x);
+
+void fead_xml(char* url);
