f820d71df656ffff62bb0c24aedf9b51eb90d72e
William Brawner <me@wbrawner.com>
2020-07-30 11:04:33 -0700

    Update schema to use more inclusive and snake-case column names
    Signed-off-by: William Brawner <me@wbrawner.com>

--- src/schema.sql
+++ src/schema.sql
@@ -2,23 +2,23 @@
     id int PRIMARY KEY, \
     title text, \
     url text, \
-    feedId int, \
+    feed_id int, \
     author text, \
-    isFavorite int, \
-    featuredImage text, \
+    is_favorite int, \
+    featured_image text, \
     content text, \
     excerpt text, \
-    isRead int, \
-    pubDate date \
+    is_read int, \
+    published_date date \
 ); \
 CREATE TABLE IF NOT EXISTS feeds ( \
     id int PRIMARY KEY, \
     title text, \
-    url text, \
-    feedUrl text, \
+    site_url text, \
+    feed_url text, \
     icon text, \
-    isFavorite int, \
-    lastPolled date, \
-    whitelist text, \
-    blacklist text \
+    is_favorite int, \
+    last_polled date, \
+    filter_accept text, \
+    filter_reject text \
 );"
